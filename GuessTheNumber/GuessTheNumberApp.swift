bc361146d5bbbc27f0128cfe36c7f2427f42a164
soloexploder <soloexploder@gmail.com>
2023-08-26 16:02:41 +0300

    Initial Commit

---
+++ GuessTheNumber/GuessTheNumberApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheNumberApp.swift
+//  GuessTheNumber
+//
+//  Created by Han Solo on 26.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheNumberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
